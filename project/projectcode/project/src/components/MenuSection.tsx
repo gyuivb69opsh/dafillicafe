2106aa37b6da32d7ee519a60d8927e4af31adb32
gyuivb69opsh <gyuivb69@gmail.com>
2026-09-18 16:21:26 +0500

    Update MenuSection.tsx

--- project/projectcode/project/src/components/MenuSection.tsx
+++ project/projectcode/project/src/components/MenuSection.tsx
@@ -19,7 +19,7 @@ export function MenuSection({ category, products }: MenuSectionProps) {
       </div>
       <div
         className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2.5 md:gap-5"
-        style={{ contentVisibility: 'auto', containIntrinsicSize: '0 320px' }}
+
       >
         {products.map((p) => (
           <ProductCard key={p.id} product={p} />
